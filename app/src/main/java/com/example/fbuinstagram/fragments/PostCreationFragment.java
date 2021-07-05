8f1bcca7be27441064b493617c2a5574acfbd532
juanlopez713 <juanlpz713@gmail.com>
2021-07-05 00:54:27 -0700

    Fixed the post creation fragment

--- app/src/main/java/com/example/fbuinstagram/fragments/PostCreationFragment.java
+++ app/src/main/java/com/example/fbuinstagram/fragments/PostCreationFragment.java
@@ -1,14 +1,22 @@
 package com.example.fbuinstagram.fragments;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
@@ -25,6 +33,7 @@ import android.widget.Toast;
 
 
 import com.example.fbuinstagram.activities.PostingActivity;
+import com.example.fbuinstagram.databinding.ActivityMainBinding;
 import com.example.fbuinstagram.databinding.FragmentPostCreationBinding;
 import com.example.fbuinstagram.models.Post;
 import com.parse.FindCallback;
@@ -49,7 +58,7 @@ public class PostCreationFragment extends Fragment {
     private static final String TAG = "PostCreationFragment";
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     FragmentPostCreationBinding binding;
     private EditText etDescription;
     private Button btnCaptureImage;
@@ -57,6 +66,9 @@ public class PostCreationFragment extends Fragment {
     private Button btnSubmit;
     private File photoFile;
     private String photoFileName;
+
+    Uri fileProvider;
+    ActivityResultLauncher<Intent> cameraResultLauncher;
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -86,10 +98,33 @@ public class PostCreationFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+
+        //To launch intent to gallery + handle it later --> gets the image from the photoFile!
+        cameraResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        Log.d(TAG, "onActivityResult: " + result.getResultCode() + " " + Activity.RESULT_OK);
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                            if (ivPostImage != null) {
+                                ivPostImage.setImageBitmap(takenImage);
+                                //  imagePosted = true;
+                            } else {
+                                //   imagePosted = false;
+                                Log.e(TAG, "imageView was null!");
+                            }
+                        } else {
+                            //    imagePosted = false;
+                            Log.d(TAG, "picture wasn't taken");
+                        }
+                    }
+                });
     }
 
     @Override
@@ -108,10 +143,30 @@ public class PostCreationFragment extends Fragment {
         btnCaptureImage = binding.btnCaptureImage;
         ivPostImage = binding.ivPostImage;
         btnSubmit = binding.btnSubmit;
+
+
+        //1a.) Initialize a file to store the taken pictures:
+        photoFile = getPhotoFileUri(photoFileName);          // creates a file reference for future access!
+        //Encapsulate photoFile in fileprovider (Parcelable) --> able to be passed into an Intent!
+        fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", photoFile);
+
         btnCaptureImage.setOnClickListener(new View.OnClickListener() {
+            //            @Override
+//            public void onClick(View v) {
+//                launchCamera();
+//            }
             @Override
             public void onClick(View v) {
-                launchCamera();
+                int permissionCheck = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE);
+                //Log.d(TAG, "onClick: " + permissionCheck);
+                //Do we have the permission to read the gallery --> go intent to gallery
+                if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                    launchCamera();
+                } else {
+                    //Request for the permission --> check permission again
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+                   btnCaptureImage.callOnClick();
+                }
             }
         });
 
@@ -135,41 +190,46 @@ public class PostCreationFragment extends Fragment {
     }
 
     private void launchCamera() {
-        // create Intent to take a picture and return control to the calling application
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Create a File reference for future access
-        photoFile = getPhotoFileUri(photoFileName);
-
-        // wrap File object into a content provider
-        // required for API >= 24
-        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
-        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", photoFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
-            // Start the image capture intent to take photo
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        }
+//        // create Intent to take a picture and return control to the calling application
+//        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        // Create a File reference for future access
+//        photoFile = getPhotoFileUri(photoFileName);
+//
+//        // wrap File object into a content provider
+//        // required for API >= 24
+//        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+//        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", photoFile);
+//        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+//
+//        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+//        // So as long as the result is not null, it's safe to use the intent.
+//        if (intent.resolveActivity(getContext().getPackageManager()) != null) {
+//            // Start the image capture intent to take photo
+//            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+//        }
+        //Initialize intent to go to camera for taking a pic --> pass in the file to store the taken pic in --> launch intent
+        Intent toCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+       toCamera.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+       //        photoFile = getPhotoFileUri(photoFileName);
+        cameraResultLauncher.launch(toCamera);
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            if (resultCode == Activity.RESULT_OK) {
-                // by this point we have the camera photo on disk
-                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
-                // RESIZE BITMAP, see section below
-                // Load the taken image into a preview
-                ivPostImage.setImageBitmap(takenImage);
-            } else { // Result was a failure
-                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+//            if (resultCode == Activity.RESULT_OK) {
+//                // by this point we have the camera photo on disk
+//                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+//                // RESIZE BITMAP, see section below
+//                // Load the taken image into a preview
+//                ivPostImage.setImageBitmap(takenImage);
+//            } else { // Result was a failure
+//                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
 
     // Returns the File for a photo stored on disk given the fileName
 
