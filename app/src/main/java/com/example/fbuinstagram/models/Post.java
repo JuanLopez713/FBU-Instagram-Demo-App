10675400f532f4caae3ca397a2f2cf0c0b8fe0c2
juanlopez713 <juanlpz713@gmail.com>
2021-07-05 21:09:34 -0700

    time now shows a relative timestamp for the post created date

--- app/src/main/java/com/example/fbuinstagram/models/Post.java
+++ app/src/main/java/com/example/fbuinstagram/models/Post.java
@@ -1,14 +1,24 @@
 package com.example.fbuinstagram.models;
 
+import android.icu.text.SimpleDateFormat;
+import android.net.ParseException;
+import android.os.Build;
+import android.text.format.DateUtils;
+import android.util.Log;
+
+import androidx.annotation.RequiresApi;
+
 import com.parse.ParseClassName;
 import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
 import java.util.Date;
+import java.util.Locale;
 
 @ParseClassName("Post")
 public class Post extends ParseObject {
+    private static final String TAG = "Post";
     //description
     public static final String KEY_DESCRIPTION = "description";
     //image
@@ -43,12 +53,29 @@ public class Post extends ParseObject {
         put(KEY_USER, user);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public String getTimeCreatedAt(Date date) {
         String stringDate = String.valueOf(date);
-        String firstPart = stringDate.substring(0, 10);
-        String secondPart = stringDate.substring(24, 28);
+    // String firstPart = stringDate.substring(0, 10);
+    // String secondPart = stringDate.substring(24, 28);
+        Log.d(TAG, "getTimeCreatedAt: "+ stringDate);
+        String twitterFormat = "EEE MMM dd HH:mm:ss zzz yyyy";
+        Log.d(TAG, "getTimeCreatedAt: " + twitterFormat);
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
 
-        return firstPart +", "+ secondPart;
+        String relativeDate = "";
+        try {
+            long dateMillis = sf.parse(stringDate).getTime();
+            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS).toString();
+        } catch (ParseException | java.text.ParseException e) {
+            e.printStackTrace();
+        }
+
+        return relativeDate;
+
+       // return firstPart +", "+ secondPart;
     }
 
 
